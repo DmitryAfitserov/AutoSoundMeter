de8292eade39edeacd1c2975418e9eb68c2269ff
Dmitry <dmitryafitserov@gmail.com>
2020-03-21 17:26:32 +0300

    created DaoSave with all methods

--- app/src/main/java/com/example/soundlevelmeter/Room/DaoSave.java
+++ app/src/main/java/com/example/soundlevelmeter/Room/DaoSave.java
@@ -22,11 +22,20 @@ public interface DaoSave {
 //    @Query("SELECT * FROM SAVE WHERE id = :idSave")
 //    LiveData<EventList> getSaveWithList(int idSave);
 
+    @Insert
+    void addListEvent(List<EventPoint> list);
 
+    @Query("SELECT * FROM event_point WHERE idSave = :idSave")
+    List<EventPoint> getListEvent(int idSave);
+
+    @Query("SELECT * FROM event_point")
+    List<EventPoint> getAllListEvent();
+
+    @Query("DELETE FROM Save WHERE id = :id")
+    void deleteSave(int id);
 
     @Insert
     void addSave(Save save);
 
-
-
+    
 }
--- app/src/main/java/com/example/soundlevelmeter/Room/EventPoint.java
+++ app/src/main/java/com/example/soundlevelmeter/Room/EventPoint.java
@@ -3,15 +3,16 @@ package com.example.soundlevelmeter.Room;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
 
 
 import static androidx.room.ForeignKey.CASCADE;
 
 @Entity(foreignKeys = @ForeignKey(entity = Save.class,
-        parentColumns = "id", childColumns = "idSave", onDelete = CASCADE, onUpdate = CASCADE), primaryKeys = {"idPoint"}, tableName = "event_point")
+        parentColumns = "id", childColumns = "idSave", onDelete = CASCADE, onUpdate = CASCADE), tableName = "event_point")
 public class EventPoint {
 
-
+    @PrimaryKey(autoGenerate = true)
     public long idPoint;
 
     @ColumnInfo(name = "idSave", index = true)
